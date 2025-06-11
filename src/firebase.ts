de4ea85a57f14f54261093d53a8d9eee0ebeb885
SayahKhalil <sayahkhalil2001@gmail.com>
2025-06-11 13:52:43 +0200

    fixing database url

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,8 +8,10 @@ const firebaseConfig = {
   storageBucket: "octopouce-fdb85.firebasestorage.app",
   messagingSenderId: "689046316349",
   appId: "1:689046316349:web:6933dbb00324934c58d154",
-  measurementId: "G-33CLMGB1E7"
+  measurementId: "G-33CLMGB1E7",
+  databaseURL: "https://octopouce-fdb85-default-rtdb.europe-west1.firebasedatabase.app" // ✅ AJOUTÉ
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
